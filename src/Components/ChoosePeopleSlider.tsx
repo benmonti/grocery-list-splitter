ab75ac2c731416340e12139559f61900d832e2ea
Ben Montimurro <benmonti@udel.edu>
2025-10-13 12:06:51 -0400

    removed unnecessary lines

--- src/Components/ChoosePeopleSlider.tsx
+++ src/Components/ChoosePeopleSlider.tsx
@@ -1,4 +1,3 @@
-/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import { useState, ChangeEvent } from "react";
 import { Item } from "../interfaces/item";
 import { updatePeople } from "../interfaces/groceryList";
