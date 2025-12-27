c109834d6612f553386b33deae620e0180e84769
Ben Montimurro <benmonti@udel.edu>
2025-12-27 06:53:17 -0500

    Updates

--- src/interfaces/people.ts
+++
@@ -1,8 +0,0 @@
-export interface Person {
-    total: number;
-    name: string;
-}
-
-export function createNewPerson(name?: string) {
-    return { name: name ? name : "", total: 0 };
-}
--- src/url.ts
+++
@@ -1,1 +0,0 @@
-export const BACKEND_URL = "https://grocery-list-split.onrender.com";
